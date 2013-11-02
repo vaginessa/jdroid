6a5c8d1f03d1d441fb2d0e8c67f17b9f97a2c118
Maxi Rosson <maxirosson@gmail.com>
2013-11-02 01:29:24 -0300

    Added default admin controller (with just a minimum of security for now)

--- jdroid-sample-server/src/main/java/com/jdroid/sample/api/admin/controller/AdminController.java
+++ jdroid-sample-server/src/main/java/com/jdroid/sample/api/admin/controller/AdminController.java
@@ -2,38 +2,12 @@ package com.jdroid.sample.api.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.jdroid.java.context.GitContext;
-import com.jdroid.java.http.MimeType;
-import com.jdroid.javaweb.context.DefaultApplication;
+import com.jdroid.javaweb.api.DefaultAdminController;
 
 /**
  * @author Maxi Rosson
  */
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
-	
-	@RequestMapping(value = "/info", method = RequestMethod.GET, produces = MimeType.TEXT)
-	@ResponseBody
-	public String getAppInfo() {
-		
-		StringBuilder builder = new StringBuilder();
-		builder.append("App Name: ");
-		builder.append(DefaultApplication.get().getDefaultApplicationContext().getAppName());
-		builder.append("\nApp Version: ");
-		builder.append(DefaultApplication.get().getDefaultApplicationContext().getAppVersion());
-		builder.append("\nCommit Id: ");
-		builder.append(GitContext.get().getCommitId());
-		builder.append("\nCommit Time: ");
-		builder.append(GitContext.get().getCommitTime());
-		builder.append("\nCommit Build Time: ");
-		builder.append(GitContext.get().getBuildTime());
-		builder.append("\nHttp Mock Enabled: ");
-		builder.append(DefaultApplication.get().getDefaultApplicationContext().isHttpMockEnabled());
-		builder.append("\nHttp Mock Sleep Duration: ");
-		builder.append(DefaultApplication.get().getDefaultApplicationContext().getHttpMockSleepDuration());
-		return builder.toString();
-	}
+public class AdminController extends DefaultAdminController {
 }
