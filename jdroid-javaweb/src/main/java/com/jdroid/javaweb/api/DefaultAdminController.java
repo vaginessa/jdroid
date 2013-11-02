11182abe224fb7ef404885e3c29448e00d1b5fef
Maxi Rosson <maxirosson@gmail.com>
2013-11-02 16:35:53 -0300

    Added security filter to all admin api services

--- jdroid-javaweb/src/main/java/com/jdroid/javaweb/api/DefaultAdminController.java
+++ jdroid-javaweb/src/main/java/com/jdroid/javaweb/api/DefaultAdminController.java
@@ -3,7 +3,6 @@ package com.jdroid.javaweb.api;
 import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.Map.Entry;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,39 +16,32 @@ import com.jdroid.javaweb.context.DefaultApplication;
  */
 public class DefaultAdminController {
 	
-	@RequestMapping(value = "/info/{key}", method = RequestMethod.GET, produces = MimeType.TEXT)
+	@RequestMapping(value = "/info", method = RequestMethod.GET, produces = MimeType.TEXT)
 	@ResponseBody
-	public String getAppInfo(@PathVariable String key) {
+	public String getAppInfo() {
+		
+		Map<String, Object> infoMap = Maps.newLinkedHashMap();
+		infoMap.put("App Name", DefaultApplication.get().getDefaultApplicationContext().getAppName());
+		infoMap.put("App Version", DefaultApplication.get().getDefaultApplicationContext().getAppVersion());
+		infoMap.put("Commit Id", GitContext.get().getCommitId());
+		infoMap.put("Commit Time", GitContext.get().getCommitTime());
+		infoMap.put("Commit Build Time", GitContext.get().getBuildTime());
+		infoMap.put("Http Mock Enabled", DefaultApplication.get().getDefaultApplicationContext().isHttpMockEnabled());
+		infoMap.put("Http Mock Sleep Duration",
+			DefaultApplication.get().getDefaultApplicationContext().getHttpMockSleepDuration());
+		infoMap.put("Default Charset", Charset.defaultCharset());
+		infoMap.put("File Encoding", System.getProperty("file.encoding"));
+		
+		infoMap.putAll(getCustomInfoMap());
 		
 		StringBuilder builder = new StringBuilder();
-		if (key.equals(getPrivateKey())) {
-			Map<String, Object> infoMap = Maps.newLinkedHashMap();
-			infoMap.put("App Name", DefaultApplication.get().getDefaultApplicationContext().getAppName());
-			infoMap.put("App Version", DefaultApplication.get().getDefaultApplicationContext().getAppVersion());
-			infoMap.put("Commit Id", GitContext.get().getCommitId());
-			infoMap.put("Commit Time", GitContext.get().getCommitTime());
-			infoMap.put("Commit Build Time", GitContext.get().getBuildTime());
-			infoMap.put("Http Mock Enabled",
-				DefaultApplication.get().getDefaultApplicationContext().isHttpMockEnabled());
-			infoMap.put("Http Mock Sleep Duration",
-				DefaultApplication.get().getDefaultApplicationContext().getHttpMockSleepDuration());
-			infoMap.put("Default Charset", Charset.defaultCharset());
-			infoMap.put("File Encoding", System.getProperty("file.encoding"));
-			
-			infoMap.putAll(getCustomInfoMap());
-			
-			for (Entry<String, Object> entry : infoMap.entrySet()) {
-				builder.append("\n");
-				builder.append(entry.getKey());
-				builder.append(": ");
-				builder.append(entry.getValue());
-			}
+		for (Entry<String, Object> entry : infoMap.entrySet()) {
+			builder.append("\n");
+			builder.append(entry.getKey());
+			builder.append(": ");
+			builder.append(entry.getValue());
 		}
 		return builder.toString();
-	}
-	
-	protected String getPrivateKey() {
-		return "private1234";
 	}
 	
 	protected Map<String, Object> getCustomInfoMap() {
---
+++ jdroid-javaweb/src/main/java/com/jdroid/javaweb/filter/AdminSecurityFilter.java
@@ -0,0 +1,43 @@
+package com.jdroid.javaweb.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.jdroid.java.utils.LoggerUtils;
+import com.jdroid.javaweb.context.DefaultApplication;
+
+/**
+ * 
+ * @author Maxi Rosson
+ */
+public class AdminSecurityFilter extends OncePerRequestFilter {
+	
+	private static final Logger LOGGER = LoggerUtils.getLogger(AdminSecurityFilter.class);
+	
+	private static final String TOKEN_PARAMETER = "token";
+	
+	/**
+	 * @see org.springframework.web.filter.OncePerRequestFilter#doFilterInternal(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = request.getParameter(TOKEN_PARAMETER);
+		if (getAdminToken().equals(token)) {
+			filterChain.doFilter(request, response);
+		} else {
+			response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED);
+			LOGGER.warn("Invalid or missing admin security token");
+		}
+	}
+	
+	protected String getAdminToken() {
+		return DefaultApplication.get().getDefaultApplicationContext().getAdminToken();
+	}
+}
