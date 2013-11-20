95aa3733ee1768bd57413e0046778b460291a18f
Maxi Rosson <maxirosson@gmail.com>
2013-11-19 22:57:41 -0300

    XMLParser improvement to filter not supported xml characters

--- jdroid-java/src/main/java/com/jdroid/java/parser/xml/XmlParser.java
+++ jdroid-java/src/main/java/com/jdroid/java/parser/xml/XmlParser.java
@@ -1,5 +1,6 @@
 package com.jdroid.java.parser.xml;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.slf4j.Logger;
@@ -44,11 +45,12 @@ public abstract class XmlParser extends DefaultHandler implements Parser {
 		
 		LOGGER.debug("Parsing started.");
 		try {
+			String content = FileUtils.toString(inputStream);
 			if (LOGGER.isDebugEnabled()) {
-				inputStream = FileUtils.copy(inputStream);
-				LOGGER.debug(FileUtils.toString(inputStream));
-				inputStream.reset();
+				LOGGER.debug(content);
 			}
+			content = stripNonValidXMLCharacters(content);
+			inputStream = new ByteArrayInputStream(content.getBytes());
 			
 			// Parse the xml
 			XMLReader reader = new SAXParser();
@@ -65,6 +67,31 @@ public abstract class XmlParser extends DefaultHandler implements Parser {
 		} finally {
 			LOGGER.debug("Parsing finished.");
 		}
+	}
+	
+	/**
+	 * This method ensures that the output String has only valid XML unicode characters as specified by the XML 1.0
+	 * standard. For reference, please see <a href="http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char">the
+	 * standard</a>. This method will return an empty String if the input is null or empty.
+	 * 
+	 * @param in The String whose non-valid characters we want to remove.
+	 * @return The in String, stripped of non-valid characters.
+	 */
+	public String stripNonValidXMLCharacters(String in) {
+		StringBuffer out = new StringBuffer(); // Used to hold the output.
+		char current; // Used to reference the current character.
+		
+		if ((in == null) || ("".equals(in))) {
+			return ""; // vacancy test.
+		}
+		for (int i = 0; i < in.length(); i++) {
+			current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+			if ((current == 0x9) || (current == 0xA) || (current == 0xD) || ((current >= 0x20) && (current <= 0xD7FF))
+					|| ((current >= 0xE000) && (current <= 0xFFFD)) || ((current >= 0x10000) && (current <= 0x10FFFF))) {
+				out.append(current);
+			}
+		}
+		return out.toString();
 	}
 	
 	/**
