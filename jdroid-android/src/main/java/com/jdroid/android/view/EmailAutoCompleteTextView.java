670af92db5eace916900f664b4b7756a7c149e63
Maxi Rosson <maxirosson@gmail.com>
2014-03-24 00:39:02 -0300

    Improved EmailAutoCompleteTextView to display the emails on the first click

--- jdroid-android/src/main/java/com/jdroid/android/view/EmailAutoCompleteTextView.java
+++ jdroid-android/src/main/java/com/jdroid/android/view/EmailAutoCompleteTextView.java
@@ -1,8 +1,10 @@
 package com.jdroid.android.view;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -34,9 +36,30 @@ public class EmailAutoCompleteTextView extends AutoCompleteTextView {
 			ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
 					android.R.layout.simple_dropdown_item_1line, AndroidUtils.getAccountsEmails());
 			setAdapter(adapter);
-			setThreshold(1);
-			setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+			setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+					| InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
 			setImeOptions(EditorInfo.IME_ACTION_NEXT);
+		}
+	}
+	
+	/**
+	 * @see android.widget.AutoCompleteTextView#enoughToFilter()
+	 */
+	@Override
+	public boolean enoughToFilter() {
+		return true;
+	}
+	
+	/**
+	 * @see android.widget.AutoCompleteTextView#onFocusChanged(boolean, int, android.graphics.Rect)
+	 */
+	@Override
+	protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+		super.onFocusChanged(focused, direction, previouslyFocusedRect);
+		
+		if ((getWindowVisibility() != View.GONE) && focused) {
+			performFiltering(getText(), 0);
+			showDropDown();
 		}
 	}
 	
