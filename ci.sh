ce41d37aa6dc3aaf48fbd878fe367dadf68ab4dc
Maxi Rosson <maxirosson@gmail.com>
2014-08-22 20:49:38 -0300

    Added continuous integration script

---
+++ ci.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Note the set -ev at the top. The -e flag causes the script to exit as soon as one command 
+# returns a non-zero exit code. The -v flag makes the shell print all lines in the script 
+# before executing them, which helps identify which steps failed.
+set -ev
+
+# ************************
+# Tests Execution
+# ************************
+
+mvn clean install -P jdroid-test
+
+# ************************
+# Deploy Snapshot
+# ************************
+
+mvn clean install -P jdroid-release -Dmaven.test.skip=true
+### Replace with this
+#mvn clean deploy -P jdroid-release -Dmaven.test.skip=true
+
+# ************************
+# jdroid sample android
+# ************************
+
+cd jdroid-sample-android
+mvn clean test -P jdroid-test
+mvn clean install -P jdroid-sample-android-uat -Dmaven.test.skip=true
+### Upload to Dropbox
+
+# ************************
+# jdroid sample server
+# ************************
+
+cd ../jdroid-sample-server
+mvn clean test -P jdroid-test
+mvn clean assembly:assembly -P jdroid-sample-server-uat -Dmaven.test.skip=true
+### Deploy on Heroku Staging
--- jdroid-java/src/main/java/com/jdroid/java/utils/ValidationUtils.java
+++ jdroid-java/src/main/java/com/jdroid/java/utils/ValidationUtils.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
  */
 public class ValidationUtils {
 	
-	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9\\+-]+(\\.[_A-Za-z0-9\\+-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*((\\.[A-Za-z]{2,}){1}$)");
+	public static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9\\+-]+(\\.[_A-Za-z0-9\\+-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*((\\.[A-Za-z]{2,}){1}$)");
 	
 	/**
 	 * Validate the email address
@@ -36,6 +36,25 @@ public class ValidationUtils {
 			new URL(value);
 			return true;
 		} catch (MalformedURLException ex) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Validate the URL without protocol
+	 * 
+	 * @param value The URL to validate
+	 * @return True if the value is a valid URL
+	 */
+	public static boolean isValidURLWithoutProtocol(String value) {
+		return isValidURL("http://" + value);
+	}
+	
+	public static boolean isValidDouble(String number) {
+		try {
+			Double.parseDouble(number);
+			return true;
+		} catch (Exception e) {
 			return false;
 		}
 	}
