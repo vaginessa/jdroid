375bbf3c6b74e1f8523b8a6465dad4c175c63798
Maxi Rosson <maxirosson@gmail.com>
2015-02-03 18:34:15 -0300

    Added android increment version tasks to gradle plugin

--- jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidApplicationGradlePlugin.groovy
+++ jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidApplicationGradlePlugin.groovy
@@ -29,13 +29,76 @@ public class AndroidApplicationGradlePlugin extends AndroidGradlePlugin {
 			println "Methods count report generated on " + outputFile.getAbsolutePath();
 
 		}
+
+		// Increment version tasks
+
+		project.task('incrementMajorVersion') << {
+
+			description = 'Increments the major version (X+1.X.X) '
+
+			project.jdroid.versionMajor = changeVersion(project, "versionMajor", null)
+			project.jdroid.versionMinor = changeVersion(project, "versionMinor", 0)
+			project.jdroid.versionPatch = changeVersion(project, "versionPatch", 0)
+			commitVersionChange(project)
+		}
+
+		project.task('incrementMinorVersion') << {
+
+			description = 'Increments the minor version (X.X+1.X)'
+
+			project.jdroid.versionMinor = changeVersion(project, "versionMinor", null)
+			project.jdroid.versionPatch = changeVersion(project, "versionPatch", 0)
+			commitVersionChange(project)
+		}
+
+		project.task('incrementPatchVersion') << {
+
+			description = 'Increments the patch version (X.X.X+1)'
+
+			project.jdroid.versionPatch = changeVersion(project, "versionPatch", null)
+			commitVersionChange(project)
+		}
+
 	}
 
 	protected Class<? extends AndroidGradlePluginExtension> getExtensionClass() {
 		return AndroidApplicationGradlePluginExtension.class;
 	}
+
+	def changeVersion(Project project, def versionType, def newVersion) {
+		def file = project.file("./build.gradle")
+		def patternVersionNumber = java.util.regex.Pattern.compile(versionType + " = (\\d+)")
+		def manifestText = file.getText()
+		def matcherVersionNumber = patternVersionNumber.matcher(manifestText)
+		matcherVersionNumber.find()
+		def currentVersion = Integer.parseInt(matcherVersionNumber.group(1))
+		if (newVersion == null) {
+			newVersion = currentVersion + 1
+		}
+		def fileContent = matcherVersionNumber.replaceAll(versionType + " = " + newVersion)
+		file.write(fileContent)
+		return newVersion
+	}
+
+	void commitVersionChange(Project project) {
+		project.exec {
+			commandLine 'git', 'diff', 'HEAD'
+		}
+
+		project.exec {
+			commandLine 'git', 'add', '-A'
+		}
+
+		project.exec {
+			commandLine 'git', 'commit', '-m', "Changed app version to v${project.jdroid.versionMajor}.${project.jdroid.versionMinor}.${project.jdroid.versionPatch}"
+		}
+	}
 }
 
 public class AndroidApplicationGradlePluginExtension extends AndroidGradlePluginExtension {
+
+	def versionMajor
+	def versionMinor
+	def versionPatch
 
 }
