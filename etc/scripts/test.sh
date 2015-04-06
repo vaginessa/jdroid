b2b66a0150fb3ddf5b5d2193a625f5a5d8dba793
Maxi Rosson <maxirosson@gmail.com>
2015-04-06 11:55:29 -0300

    Adding verbose flag to test.sh

--- etc/scripts/test.sh
+++ etc/scripts/test.sh
@@ -3,7 +3,7 @@
 # Note the set -ev at the top. The -e flag causes the script to exit as soon as one command
 # returns a non-zero exit code. The -v flag makes the shell print all lines in the script
 # before executing them, which helps identify which steps failed.
-set -e
+set -ev
 
 UPLOAD=$1
 
