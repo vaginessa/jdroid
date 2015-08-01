8ff424bfab6af83d33a466039a0c48332aa77e1d
Maxi Rosson <maxirosson@gmail.com>
2015-08-01 11:22:53 -0300

    Renamed samples. Fix build

--- etc/scripts/test.sh
+++ etc/scripts/test.sh
@@ -19,16 +19,16 @@ fi
 ./gradlew :jdroid-java:clean :jdroid-java:uploadArchives :jdroid-gradle-plugin:clean :jdroid-gradle-plugin:uploadArchives --configure-on-demand -PLOCAL_UPLOAD=true
 
 # ************************
-# jdroid sample server
+# jdroid javaweb sample
 # ************************
 
-./gradlew :jdroid-java:clean :jdroid-java:build :jdroid-java:test :jdroid-java-http-apache:build :jdroid-java-http-apache:test :jdroid-java-http-urlconnection:build :jdroid-java-http-urlconnection:test :jdroid-java-http-okhttp:build :jdroid-java-http-okhttp:test :jdroid-javaweb:clean :jdroid-javaweb:build :jdroid-javaweb:test :jdroid-sample-server:build --configure-on-demand
+./gradlew :jdroid-java:clean :jdroid-java:build :jdroid-java:test :jdroid-java-http-apache:build :jdroid-java-http-apache:test :jdroid-java-http-urlconnection:build :jdroid-java-http-urlconnection:test :jdroid-java-http-okhttp:build :jdroid-java-http-okhttp:test :jdroid-javaweb:clean :jdroid-javaweb:build :jdroid-javaweb:test :jdroid-javaweb-sample:build --configure-on-demand
 
 # ************************
-# jdroid sample android
+# jdroid android sample
 # ************************
 
-#./gradlew :jdroid-android:clean :jdroid-android:build :jdroid-android:testDebug :jdroid-android-facebook:clean :jdroid-android-facebook:build :jdroid-android-facebook:testDebug :jdroid-android-google-maps:clean :jdroid-android-google-maps:build :jdroid-android-google-maps:testDebug :jdroid-android-google-gcm:clean :jdroid-android-google-gcm:build :jdroid-android-google-gcm:testDebug :jdroid-android-google-plus:clean :jdroid-android-google-plus:build :jdroid-android-google-plus:testDebug :jdroid-android-about:clean :jdroid-android-about:build :jdroid-android-about:testDebug :jdroid-sample-android:check :jdroid-sample-android:assembleUat :jdroid-sample-android:countMethodsSummary
+#./gradlew :jdroid-android:clean :jdroid-android:build :jdroid-android:testDebug :jdroid-android-facebook:clean :jdroid-android-facebook:build :jdroid-android-facebook:testDebug :jdroid-android-google-maps:clean :jdroid-android-google-maps:build :jdroid-android-google-maps:testDebug :jdroid-android-google-gcm:clean :jdroid-android-google-gcm:build :jdroid-android-google-gcm:testDebug :jdroid-android-google-plus:clean :jdroid-android-google-plus:build :jdroid-android-google-plus:testDebug :jdroid-android-about:clean :jdroid-android-about:build :jdroid-android-about:testDebug :jdroid-android-sample:check :jdroid-android-sample:assembleUat :jdroid-android-sample:countMethodsSummary
 
 # ************************
 # Upload Snapshot to Maven repository
