ffc73e6a10773206d5f8822e5dacb194e0d6695a
Maxi Rosson <maxirosson@gmail.com>
2015-10-30 01:31:56 -0300

    Removed heroku support on grade plugin

--- jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/javaweb/JavaWebGradlePlugin.groovy
+++ jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/javaweb/JavaWebGradlePlugin.groovy
@@ -1,8 +1,5 @@
 package com.jdroid.gradle.javaweb
-
 import com.jdroid.gradle.java.JavaGradlePlugin
-import com.jdroid.gradle.javaweb.task.CopyJettyRunnerTask
-import com.jdroid.gradle.javaweb.task.HerokuBuildTask
 import org.gradle.api.Project
 
 public class JavaWebGradlePlugin extends JavaGradlePlugin {
@@ -11,20 +8,6 @@ public class JavaWebGradlePlugin extends JavaGradlePlugin {
 		super.apply(project)
 
 		project.apply plugin: 'jetty'
-
-		project.configurations {
-			runtimeOnly
-		}
-
-		project.dependencies {
-			// to run our App on Heroku
-			runtimeOnly "org.mortbay.jetty:jetty-runner:8.1.1.v20120215"
-		}
-
-
-		project.task('copyJettyRunner', type: CopyJettyRunnerTask)
-		project.task('stage', type: HerokuBuildTask)
-		project.tasks.'stage'.dependsOn 'assemble', 'copyJettyRunner'
 	}
 
 	protected Class<? extends JavaWebGradleExtension> getExtensionClass() {
