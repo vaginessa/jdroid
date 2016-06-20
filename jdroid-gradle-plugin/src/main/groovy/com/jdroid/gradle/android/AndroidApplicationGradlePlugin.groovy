21be7511e8b771ba2dac7ec65d6de5f4e6db698f
Maxi Rosson <maxirosson@gmail.com>
2016-06-19 21:32:48 -0300

    Removed DEX methods counter (new Android Studio has these feature included). Removed Google Play Publisher (new external jdroid plugin in progress)

--- jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidApplicationGradlePlugin.groovy
+++ jdroid-gradle-plugin/src/main/groovy/com/jdroid/gradle/android/AndroidApplicationGradlePlugin.groovy
@@ -2,20 +2,12 @@ package com.jdroid.gradle.android
 
 import com.android.build.gradle.AppPlugin
 import com.jdroid.gradle.android.task.CopyApksTask
-import com.jdroid.gradle.android.task.CountMethodsReportTask
-import com.jdroid.gradle.android.task.CountMethodsSummaryTask
-import com.jdroid.gradle.android.task.GooglePlayPublisherTask
 import org.gradle.api.Project
 
 public class AndroidApplicationGradlePlugin extends AndroidGradlePlugin {
 
 	public void apply(Project project) {
 		super.apply(project);
-
-		project.task('countMethodsReport', type: CountMethodsReportTask)
-		project.task('countMethodsSummary', type: CountMethodsSummaryTask)
-
-		project.task('googlePlayPublish', type: GooglePlayPublisherTask)
 
 		project.task('copyApks', type: CopyApksTask)
 
