c8ddee825a07d1b31514b91c80f5541c631377b5
Maxi Rosson <maxirosson@gmail.com>
2016-10-24 01:43:02 -0300

    Upgrade to Support Library v25.0.0

--- jdroid-android/src/main/java/com/jdroid/android/utils/ReferrerUtils.java
+++ jdroid-android/src/main/java/com/jdroid/android/utils/ReferrerUtils.java
@@ -2,20 +2,18 @@ package com.jdroid.android.utils;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.ParseException;
 import android.net.Uri;
-import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 
 import com.google.android.gms.appindexing.AndroidAppUri;
 
 public class ReferrerUtils {
 
 	private static final String EXTRA_REFERRER = "android.intent.extra.REFERRER";
-	private static final String REFERRER_NAME = "android.intent.extra.REFERRER_NAME";
 
 	public static String getReferrerCategory(Activity activity) {
 		String referrerCategory = null;
-		Uri referrerUri = getReferrer(activity);
+		Uri referrerUri = ActivityCompat.getReferrer(activity);
 		if (referrerUri != null) {
 			if (referrerUri.getScheme().equals("http") || referrerUri.getScheme().equals("https")) {
 				referrerCategory = "http://";
@@ -39,33 +37,6 @@ public class ReferrerUtils {
 			}
 		}
 		return referrerCategory;
-	}
-
-	/** Returns the referrer who started the Activity. */
-	public static Uri getReferrer(Activity activity) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-			return activity.getReferrer();
-		}
-		return getReferrerCompatible(activity);
-	}
-
-	/** Returns the referrer on devices running SDK versions lower than 22. */
-	private static Uri getReferrerCompatible(Activity activity) {
-		Intent intent = activity.getIntent();
-		Uri referrerUri = intent.getParcelableExtra(EXTRA_REFERRER);
-		if (referrerUri != null) {
-			return referrerUri;
-		}
-		String referrer = intent.getStringExtra(REFERRER_NAME);
-		if (referrer != null) {
-			// Try parsing the referrer URL; if it's invalid, return null
-			try {
-				return Uri.parse(referrer);
-			} catch (ParseException e) {
-				return null;
-			}
-		}
-		return null;
 	}
 
 	public static void setReferrer(Intent intent, String referrer) {
