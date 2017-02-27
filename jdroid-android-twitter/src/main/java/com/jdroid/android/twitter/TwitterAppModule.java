eef6003ab16959682b4d0b55f1870564c67e5f52
Maxi Rosson <maxirosson@gmail.com>
2017-02-27 20:46:50 -0300

    Twitter configuration is now optional

--- jdroid-android-twitter/src/main/java/com/jdroid/android/twitter/TwitterAppModule.java
+++ jdroid-android-twitter/src/main/java/com/jdroid/android/twitter/TwitterAppModule.java
@@ -3,15 +3,20 @@ package com.jdroid.android.twitter;
 import com.jdroid.android.application.AbstractAppModule;
 import com.jdroid.android.application.AbstractApplication;
 import com.jdroid.java.collections.Lists;
+import com.jdroid.java.utils.LoggerUtils;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.tweetui.TweetUi;
+
+import org.slf4j.Logger;
 
 import java.util.List;
 
 import io.fabric.sdk.android.Kit;
 
 public class TwitterAppModule extends AbstractAppModule {
+
+	private final static Logger LOGGER = LoggerUtils.getLogger(TwitterAppModule.class);
 
 	public static final String MODULE_NAME = TwitterAppModule.class.getName();
 
@@ -35,8 +40,15 @@ public class TwitterAppModule extends AbstractAppModule {
 
 	@Override
 	public List<Kit> createFabricKits() {
-		TwitterAuthConfig authConfig = new TwitterAuthConfig(twitterAppContext.getTwitterOauthConsumerKey(), twitterAppContext.getTwitterOauthConsumerSecret());
-		return Lists.<Kit>newArrayList(new TwitterCore(authConfig), new TweetUi());
+		String twitterOauthConsumerKey = twitterAppContext.getTwitterOauthConsumerKey();
+		String twitterOauthConsumerSecret = twitterAppContext.getTwitterOauthConsumerSecret();
+		if (twitterOauthConsumerKey == null || twitterOauthConsumerSecret == null) {
+			LOGGER.error("Missing TWITTER_OAUTH_CONSUMER_KEY or TWITTER_OAUTH_CONSUMER_SECRET");
+			return Lists.newArrayList();
+		} else {
+			TwitterAuthConfig authConfig = new TwitterAuthConfig(twitterOauthConsumerKey, twitterOauthConsumerSecret);
+			return Lists.<Kit>newArrayList(new TwitterCore(authConfig), new TweetUi());
+		}
 	}
 
 }
