b12c44feed54c8467e6fd4595ec5cad158cbb47e
Maxi Rosson <maxirosson@gmail.com>
2017-07-16 19:23:41 -0300

    Ads config controlled by firebase remote config

--- jdroid-android-firebase-admob/src/main/java/com/jdroid/android/firebase/admob/AdMobRemoteConfigParameter.java
+++ jdroid-android-firebase-admob/src/main/java/com/jdroid/android/firebase/admob/AdMobRemoteConfigParameter.java
@@ -5,6 +5,8 @@ import com.jdroid.android.firebase.remoteconfig.RemoteConfigParameter;
 public enum AdMobRemoteConfigParameter implements RemoteConfigParameter {
 
 	ADS_ENABLED(false),
+	MIN_APP_LOADS_TO_DISPLAY_ADS(5),
+	MIN_DAYS_TO_DISPLAY_ADS(7),
 	ADMOB_APP_ID,
 	DEFAULT_AD_UNIT_ID;
 
