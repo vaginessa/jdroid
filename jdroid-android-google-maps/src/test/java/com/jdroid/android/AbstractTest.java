78abb25516136e86e93a681255c8d5ba1e49f611
Maxi Rosson <maxirosson@gmail.com>
2017-09-21 16:12:03 -0300

    Upgrade to Robolectric v3.4.2. Close #427

--- jdroid-android-google-maps/src/test/java/com/jdroid/android/AbstractTest.java
+++ jdroid-android-google-maps/src/test/java/com/jdroid/android/AbstractTest.java
@@ -10,7 +10,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, manifest=Config.NONE, application = TestAndroidApplication.class, sdk = Build.VERSION_CODES.N_MR1)
+@Config(constants = BuildConfig.class, manifest=Config.NONE, application = TestAndroidApplication.class, sdk = Build.VERSION_CODES.O)
 public abstract class AbstractTest {
 
 	@Before
